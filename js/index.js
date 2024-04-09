f623eae4b3092be1abdc99307680cc2f551edd56
constansea <150091682+constansea@users.noreply.github.com>
2024-04-09 04:49:59 -0400

    scroll animation

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.animate').scrolla();
+});
+
+$('.animate').scrolla({
+    // default
+    mobile: false, // disable animation on mobiles
+    once: false, // only once animation play on scroll
+    animateCssVersion: 4 // used animate.css version (3 or 4)
+});
